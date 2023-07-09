851f19f371fc0772d7841ede2bb3cceb1fbd7098
Daniel Silva <danielsilvatsi@outlook.com>
2023-07-08 21:30:48 -0300

    feat: add components

--- angular-login-firebase/src/app/app.module.ts
+++ angular-login-firebase/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RegisterUserComponent } from './components/register-user/register-user.component';
+import { CheckMailComponent } from './components/check-mail/check-mail.component';
+import { RecoverPasswordComponent } from './components/recover-password/recover-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    RegisterUserComponent,
+    CheckMailComponent,
+    RecoverPasswordComponent
   ],
   imports: [
     BrowserModule,
