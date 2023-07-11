88d0854e58e70ddd4126a0fe52c46feadbd69e00
Daniel Silva <danielsilvatsi@outlook.com>
2023-07-10 21:27:09 -0300

    feat: add reset email

--- src/app/components/recover-password/recover-password.component.ts
+++ src/app/components/recover-password/recover-password.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FirebaseErrorService } from './../../services/firebase-error.service';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { ToastrService } from 'ngx-toastr';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-recover-password',
   templateUrl: './recover-password.component.html',
   styleUrls: ['./recover-password.component.css']
 })
-export class RecoverPasswordComponent {
+export class RecoverPasswordComponent implements OnInit {
 
+  recoverUser!: FormGroup;
+  loading: boolean = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private afAuth: AngularFireAuth,
+    private toastr: ToastrService,
+    private router: Router,
+    private firebaseError: FirebaseErrorService
+  ) {
+    this.recoverUser = this.fb.group({
+      email: ['', Validators.required],
+    });
+  }
+  ngOnInit(): void {
+  }
+
+  Recover() {
+    const email = this.recoverUser.value.email;
+
+    this.loading = true;
+    this.afAuth.sendPasswordResetEmail(email).then(() => {
+     this.toastr.info('Escolha uma nova senha para a sua conta', 'Recuperar Senha');
+     this.router.navigate(['/login']);
+    }).catch((error) => {
+      this.loading = false;
+      this.toastr.error(this.firebaseError.codeError(error.code), 'Error');
+    });
+  }
 }
